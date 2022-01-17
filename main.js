c5ca584f8574a2f686e666b27eeb81f68d691d71
Emily <yday1223@gmail.com>
2022-01-17 14:20:52 +0900

    JS Navbar Menu Click

--- main.js
+++ main.js
@@ -10,3 +10,38 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
+
+// const home = document.querySelector('#home');
+// const about = document.querySelector('#about');
+// const skills = document.querySelector('#skills');
+// const work = document.querySelector('#work');
+// const testimonials = document.querySelector('#testimonials');
+// const contact = document.querySelector('#contact');
+
+// const topHome = home.offsetTop;
+// const topAbout = about.offsetTop;
+// const topSkills = skills.offsetTop;
+// const topWork = work.offsetTop;
+// const topTestimonials = testimonials.offsetTop;
+// const topContact = contact.offsetTop;
+
+// function autoscroll() {
+//   window.scroll({
+//     top: topContact,
+//     left: 0,
+//     behavior: 'smooth',
+//   });
+// }
