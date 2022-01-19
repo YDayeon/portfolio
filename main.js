df19a102540e46b229d7b050b618f11b271d9c67
Emily <yday1223@gmail.com>
2022-01-19 09:50:45 +0900

    JS Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -22,7 +22,15 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  console.log('toggle');
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
