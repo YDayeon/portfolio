a0a7f545bb624f1f3d876562c46f4d6b12149024
Emily <yday1223@gmail.com>
2022-02-02 11:43:56 +0900

    IntersectionObserver

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// the current position of section
+function getIndexOfSection() {
+  const section = document
+    .elementFromPoint(window.innerWidth / 2, window.innerHeight * (2 / 3))
+    .closest('.section');
+  const idx = sectionIds.indexOf(`#${section.id}`);
+  return idx;
+}
+
 //Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -17,9 +26,6 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  const active = document.querySelector('.navbar__menu__item.selected');
-  active.classList.remove('selected');
-  target.classList.add('selected');
   if (link == null) {
     return;
   }
@@ -59,25 +65,6 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Skill bar moving
-const about = document.querySelector('#about');
-const aboutHeight = about.getBoundingClientRect().height;
-const skill = document.querySelector('#skills');
-const work = document.querySelector('#work');
-const skillValue = document.querySelectorAll('.skill__value');
-const skillHeight = skill.getBoundingClientRect().height;
-const workHeight = work.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  if (
-    homeHeight + aboutHeight - 100 > window.scrollY ||
-    window.scrollY > homeHeight + aboutHeight + skillHeight
-  ) {
-    skillValue.forEach((v) => (v.style.width = 0));
-  } else {
-    skillValue.forEach((v) => (v.style.width = v.dataset.width));
-  }
-});
-
 // Show "arrow-up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', () => {
@@ -94,63 +81,57 @@ arrowUp.addEventListener('click', () => {
 });
 
 // Projects
-const workBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
+// const workBtnContainer = document.querySelector('.work__categories');
+// const projectContainer = document.querySelector('.work__projects');
+// const projects = document.querySelectorAll('.project');
+// workBtnContainer.addEventListener('click', (e) => {
+//   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+//   if (filter == null) {
+//     return;
+//   }
 
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target =
-    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-  target.classList.add('selected');
+//   // Remove selection from the previous item and select the new one
+//   const active = document.querySelector('.category__btn.selected');
+//   active.classList.remove('selected');
+//   const target =
+//     e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+//   target.classList.add('selected');
 
-  projectContainer.classList.add('anim-out');
-  setTimeout(() => {
-    projects.forEach((project) => {
-      if (filter === '*' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-    projectContainer.classList.remove('anim-out');
-  }, 250);
-});
+//   projectContainer.classList.add('anim-out');
+//   setTimeout(() => {
+//     projects.forEach((project) => {
+//       if (filter === '*' || filter === project.dataset.type) {
+//         project.classList.remove('invisible');
+//       } else {
+//         project.classList.add('invisible');
+//       }
+//     });
+//     projectContainer.classList.remove('anim-out');
+//   }, 250);
+// });
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
-
-// 1. 모든 sectron 요소들을 가지고 온다
+// 1. 모든 section 요소들과 메뉴 아이템들을 가지고 온다
 // 2. IntersectionObserver를 이용해서 모든 section들을 관찰한다.
-// 3. 보여지는 section에 해당하는 메뉴 아이템을 활성화 시킨다.testimonial
+// 3. 보여지는 section에 해당하는 메뉴 아이템을 활성화 시킨다.
 
-const sectionIds = [
-  '#home',
-  '#about',
-  '#skills',
-  '#work',
-  '#testimonials',
-  '#contact',
-];
+const sectionIds = ['#home', '#about', '#skills', '#work', '#contact'];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[data-link="${id}"]`)
 );
 
-let selectedNavIndex;
-let selectedNavItem = navItems[0];
+let selectedNavIndex = getIndexOfSection();
+let selectedNavItem = navItems[selectedNavIndex];
 function selectNavItem(selected) {
   selectedNavItem.classList.remove('selected');
   selectedNavItem = selected;
   selectedNavItem.classList.add('selected');
+}
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
 }
 
 const observerOptions = {
@@ -172,5 +153,28 @@ const observerCallback = (entries, observer) => {
     }
   });
 };
+
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach((section) => observer.observe(section));
+const skillValue = document.querySelectorAll('.skill__value');
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) ===
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  if (selectedNavIndex === 2) {
+    skillValue.forEach((v) => (v.style.width = v.dataset.width));
+  } else {
+    skillValue.forEach((v) => (v.style.width = 0));
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
+
+// F5
+window.addEventListener('load', () => {
+  selectNavItem(navItems[selectedNavIndex]);
+});
